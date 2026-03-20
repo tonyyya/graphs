83bfc2913b8470e55a8cc0315717760eee88a118
Тоня <140175414+tonyyya@users.noreply.github.com>
2026-03-20 20:41:36 +0300

    Create AdjListGraph.hpp

---
+++ src/core/AdjListGraph.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "IGraph.hpp"
+#include <vector>
+#include <stdexcept>
+
+class AdjListGraph : public IGraph {
+private:
+    std::vector<std::vector<int>> adj;
+    int edgeCount = 0;
+
+public:
+    AdjListGraph(int n = 0) : adj(n) {}
+
+    int verticesCount() const override {
+        return adj.size();
+    }
+
+    int edgesCount() const override {
+        return edgeCount;
+    }
+
+    void addEdge(int u, int v) override {
+        if (u >= verticesCount() || v >= verticesCount())
+            throw std::out_of_range("Invalid vertex");
+
+        if (hasEdge(u, v)) return;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        edgeCount++;
+    }
+
+    bool hasEdge(int u, int v) const override {
+        for (int x : adj[u])
+            if (x == v) return true;
+        return false;
+    }
+
+    const std::vector<int>& neighbors(int v) const override {
+        return adj[v];
+    }
+};
