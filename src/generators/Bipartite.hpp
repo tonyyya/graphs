5818fa8e7f7b510bea715bf040bdde66afe04587
veronikkkaa <verdmigarshina@edu.hse.ru>
2026-03-30 23:29:36 +0300

    мямям

--- src/generators/Bipartite.hpp
+++ src/generators/Bipartite.hpp
@@ -2,21 +2,19 @@
 #include "IGraphGenerator.hpp"
 #include "../core/AdjListGraph.hpp"
 
-class WheelGenerator : public IGraphGenerator {
-    int n;
+class CompleteBipartiteGenerator : public IGraphGenerator {
+    int n, m;
 
 public:
-    WheelGenerator(int n) : n(n) {}
+    CompleteBipartiteGenerator(int n, int m) : n(n), m(m) {}
 
     std::unique_ptr<IGraph> generate() const override {
-        auto g = std::make_unique<AdjListGraph>(n);
-
-        for (int i = 1; i < n; i++)
-            g->addEdge(0, i);
-
-        for (int i = 1; i < n; i++)
-            g->addEdge(i, (i % (n - 1)) + 1);
-
+        auto g = std::make_unique<AdjListGraph>(n + m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                g->addEdge(i, n + j);
+            }
+        }
         return g;
     }
 };
